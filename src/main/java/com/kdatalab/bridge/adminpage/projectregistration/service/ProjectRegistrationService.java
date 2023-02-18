b191eebea7262af8002d3488f71d6e95db1f4572
YSHUB <d.toshboltayev05@gmail.com>
2023-02-18 08:38:52 +0500

    refactoring registatiion

--- src/main/java/com/kdatalab/bridge/adminpage/projectregistration/service/ProjectRegistrationService.java
+++ src/main/java/com/kdatalab/bridge/adminpage/projectregistration/service/ProjectRegistrationService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -32,8 +31,8 @@ public class ProjectRegistrationService {
         String userId = "yanghee";//TODO userId get from session
         Integer projectId;
         if (dto.getProjectId() == null) {
-            projectId = projectRegistrationRepository.saveProject(dto.getProjectType(), dto.getProjectName(), dto.getProjectContent(), dto.getWorkDateInMinutes(),
-                    dto.getProjectStartDate(), dto.getProjectEndDate(), dto.getPointPerImage(), dto.getTaskUnit(), userId);
+            projectId = projectRegistrationRepository.saveProject(dto.getProjectType(), dto.getProjectName(), dto.getProjectContent(),
+                    dto.getWorkDateInMinutes(), dto.getProjectStartDate(), dto.getProjectEndDate(), dto.getPointPerImage(), dto.getTaskUnit(), userId);
             //generate task
             registerTask(projectId, userId, dto.getTaskUnit());
             //save image to aws s3 and image data to tb_att table
@@ -41,66 +40,72 @@ public class ProjectRegistrationService {
         } else {
             //update project
             projectId = dto.getProjectId();
-            projectRegistrationRepository.updateProject(projectId, dto.getProjectName(), dto.getProjectContent(), dto.getWorkDateInMinutes(),
-                    dto.getProjectStartDate(), dto.getProjectEndDate(), userId);
+            projectRegistrationRepository.updateProject(projectId, dto.getProjectName(), dto.getProjectContent(),
+                    dto.getWorkDateInMinutes(), dto.getProjectStartDate(), dto.getProjectEndDate(), userId);
         }
         return projectId;
     }
 
     public void attachFileToTask(Integer projectId,String projectName, Integer taskUnit, String regUser, List<MultipartFile> files) throws IOException {
         // list of tasks
-        List<Integer> taskIds = projectRegistrationRepository.getTasksByProjectId(projectId);// Arrays.asList(4,5,6);//,7,8);
+        List<Integer> taskIds = projectRegistrationRepository.getTasksByProjectId(projectId);
         int length = files.size();//17
         int taskCount = length / taskUnit;//5
         int remainder = length % taskUnit; //2
         for (int i = 0; i < taskUnit; i++) {
             int taskId = taskIds.get(i);
             for(int j = 0; j < taskCount; j++) {
-                AttachmentDto attachmentDto = new AttachmentDto();
-                attachmentDto.setProjectId(projectId); //EDU_SEQ
-                attachmentDto.setTaskId(taskId); //DTL_SEQ
-                attachmentDto.setFileName(files.get(i+j).getOriginalFilename()); //NAME
-                attachmentDto.setFilePath(generateTaskPath(projectName, taskId));//PATH
-                attachmentDto.setFileExt(files.get(i+j).getContentType());//EXT
-                attachmentDto.setFileSize(files.get(i+j).getSize());//SIZE
-                attachmentDto.setRegUser(regUser);//REG_USER
-                attachmentDto.setRegDate(LocalDateTime.now().toString());//REG_DATE
-                attachmentDto.setModUser(regUser);//MOD_USER
-                attachmentDto.setModDate(LocalDateTime.now().toString());//MOD_DATE
-                System.out.println("attachmentDto = " + attachmentDto);
+                int index = i + j;
+                AttachmentDto attachmentDto = createAttachment(projectId,
+                                                               taskId,
+                                                               files.get(index).getOriginalFilename(),
+                                                               generateTaskPath(projectName, taskId),
+                                                               files.get(index).getContentType(),
+                                                               files.get(index).getSize(),regUser);
                 //save to db image data
                 saveImageData(attachmentDto);
                 //save to s3 image
-                awsService.uploadImageToAws(files.get(i+j),attachmentDto.getFilePath());
+                awsService.uploadImageToAws(files.get(index),attachmentDto.getFilePath());
             }
         }
         for(int i = 0; i < remainder; i++) {
             int taskId = taskIds.get(i);
-            AttachmentDto attachmentDto = new AttachmentDto();
-            attachmentDto.setProjectId(projectId); //EDU_SEQ
-            attachmentDto.setTaskId(taskId); //DTL_SEQ
-            attachmentDto.setFileName(files.get(taskUnit*taskCount+i).getOriginalFilename()); //NAME
-            attachmentDto.setFilePath(generateTaskPath(projectName, taskId));//PATH
-            attachmentDto.setFileSize(files.get(taskUnit*taskCount+i).getSize());//SIZE
-            attachmentDto.setFileExt(files.get(taskUnit*taskCount+i).getContentType());//EXT
-            attachmentDto.setRegUser(regUser);//REG_USER
-            attachmentDto.setRegDate(LocalDateTime.now().toString());//REG_DATE
-            attachmentDto.setModUser(regUser);//MOD_USER
-            attachmentDto.setModDate(LocalDateTime.now().toString());//MOD_DATE
-            System.out.println("attachmentDto = " + attachmentDto);
+            int index = taskUnit*taskCount+i;
+            AttachmentDto attachmentDto = createAttachment(projectId,
+                                                           taskId,
+                                                           files.get(index).getOriginalFilename(),
+                                                           generateTaskPath(projectName, taskId),
+                                                           files.get(index).getContentType(),
+                                                           files.get(index).getSize(),regUser);
             //save to db image data
             saveImageData(attachmentDto);
             //save to aws s3 image
-            awsService.uploadImageToAws(files.get(taskUnit*taskCount+i),attachmentDto.getFilePath());
+            awsService.uploadImageToAws(files.get(index),attachmentDto.getFilePath());
         }
+    }
 
+    private AttachmentDto createAttachment(Integer projectId, Integer taskId, String fileName, String filePath,
+                                           String fileExt, Long fileSize, String regUser) {
+        AttachmentDto attachmentDto = new AttachmentDto();
+        attachmentDto.setProjectId(projectId); //EDU_SEQ
+        attachmentDto.setTaskId(taskId); //DTL_SEQ
+        attachmentDto.setFileName(fileName); //NAME
+        attachmentDto.setFilePath(filePath);//PATH
+        attachmentDto.setFileExt(fileExt);//EXT
+        attachmentDto.setFileSize(fileSize);//SIZE
+        attachmentDto.setRegUser(regUser);//REG_USER
+        return attachmentDto;
     }
 
     public void saveImageData(AttachmentDto attachmentDto) {
         //save to db image data
-        projectRegistrationRepository.saveTbAtt(attachmentDto.getProjectId(), attachmentDto.getTaskId(), attachmentDto.getFileName(),
-                attachmentDto.getFilePath(), attachmentDto.getFileSize(), attachmentDto.getFileExt(), attachmentDto.getRegUser(),
-                attachmentDto.getRegDate(), attachmentDto.getModUser(), attachmentDto.getModDate());
+        projectRegistrationRepository.saveTbAtt(attachmentDto.getProjectId(),
+                                                attachmentDto.getTaskId(),
+                                                attachmentDto.getFileName(),
+                                                attachmentDto.getFilePath(),
+                                                attachmentDto.getFileSize(),
+                                                attachmentDto.getFileExt(),
+                                                attachmentDto.getRegUser());
     }
 
     private String generateTaskPath(String projectName, Integer taskId) {
