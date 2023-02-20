a0a7ee18d3162c0b2be489866c7e4640959a95bd
IslomJon <islomjonisoqov1@gmail.com>
2023-02-20 15:24:19 +0500

    project registration edit page

--- src/main/java/com/kdatalab/bridge/adminpage/projectregistration/controller/ProjectRegistrationController.java
+++ src/main/java/com/kdatalab/bridge/adminpage/projectregistration/controller/ProjectRegistrationController.java
@@ -8,15 +8,13 @@ import com.kdatalab.bridge.user.mapper.UserMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/project-registration")
-public class ProjectRegistrationController{
+public class ProjectRegistrationController extends BaseController{
 
     private final ProjectRegistrationService service;
     private final UserMapper userService;
@@ -28,14 +26,21 @@ public class ProjectRegistrationController{
 
     @GetMapping("/step-2/{projectId}")
     @PreAuthorize("isFullyAuthenticated()")
-    public String getStep2Page(@PathVariable Long projectId, Model model){
+    public String getStep2Page(@PathVariable Long projectId, @RequestParam(required = false, defaultValue = "false") Boolean edit, Model model){
         List<TaskDto> projectWithTasks = service.getProjectWithTasks(projectId);
         List<UserDto> users = userService.selectUserByQcChk('N');
         List<UserDto> admins = userService.selectUserByQcChk('Y');
         model.addAttribute("tasks", projectWithTasks);
         model.addAttribute("users", users);
         model.addAttribute("admins", admins);
+        model.addAttribute("edited", edit);
         return "adminpage/projectRegistration2";
+    }
+
+    @PostMapping("/step-2")
+    public String saveStep2Project(List<TaskDto> dtos){
+        // TODO: 2/20/2023 save dtos
+        return "redirect: project-registration";
     }
 
 }
