f7f882715e7275eb5afd73a99ccdcf0f02779a07
Zafar Yodgorov <yodzafar9966@gmail.com>
2023-02-23 16:10:47 +0500

    leave the membership

--- src/main/java/com/kdatalab/bridge/user/dto/UserDto.java
+++ src/main/java/com/kdatalab/bridge/user/dto/UserDto.java
@@ -2,11 +2,9 @@ package com.kdatalab.bridge.user.dto;
 
 import lombok.Data;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * 사용자 정보
+ *
  * @author Enclouds
  * @since 2020.12.11
  */
@@ -25,22 +23,22 @@ public class UserDto {
     private String loginIp;
     private Character status;
 
-    public String getBirthDtFormat(){
+    public String getBirthDtFormat() {
         if (birthDt == null) {
             return "-";
         }
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        return format.format(new Date(Long.parseLong(birthDt)));
+        return birthDt.substring(0, 4) + "-" + birthDt.substring(4, 6) + "-" + birthDt.substring(6);
     }
 
-    public String getEmailStart(){
+    public String getEmailStart() {
         return eMail.substring(0, eMail.indexOf("@"));
     }
-    public String getEmailLast(){
-        return eMail.substring(eMail.indexOf("@")+1);
+
+    public String getEmailLast() {
+        return eMail.substring(eMail.indexOf("@") + 1);
     }
 
-    public boolean getEmailStatus(){
+    public boolean getEmailStatus() {
         String emailLast = getEmailLast();
         return (emailLast.equalsIgnoreCase("naver.com") || emailLast.equalsIgnoreCase("hanmail.net"));
     }
